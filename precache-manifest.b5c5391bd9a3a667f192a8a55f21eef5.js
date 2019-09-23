fb66c4e545013e23ff0a1cb154bfc1b6a64bef90
Tomasz Olędzki <luksfer@poczta.onet.pl>
2019-09-23 13:46:41 +0200

    Updates

--- precache-manifest.b5c5391bd9a3a667f192a8a55f21eef5.js
+++ precache-manifest.b5c5391bd9a3a667f192a8a55f21eef5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ce770e81102a52c9f83a38f6b439f7f",
+    "revision": "bdd852671c35b3443a795b739c48a79e",
     "url": "/parkrun/index.html"
   },
   {
-    "revision": "c5d30e217b5a18f9fcf8",
+    "revision": "a2856c6040e57e1bba9d",
     "url": "/parkrun/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/parkrun/static/js/2.655a53b8.chunk.js"
   },
   {
-    "revision": "c5d30e217b5a18f9fcf8",
-    "url": "/parkrun/static/js/main.6cd1e810.chunk.js"
+    "revision": "a2856c6040e57e1bba9d",
+    "url": "/parkrun/static/js/main.433b46a6.chunk.js"
   },
   {
     "revision": "9e41e48c7261de012d9f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/parkrun/precache-manifest.e27e5d6d131fd026306ca1fdf08a65cd.js"
+  "/parkrun/precache-manifest.b5c5391bd9a3a667f192a8a55f21eef5.js"
 );
 
 self.addEventListener('message', (event) => {
